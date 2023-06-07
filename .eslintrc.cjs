8179c20b6897e4c695854211cdbb53be20675faa
FarhanFardid <farhanfardid009@gmail.com>
2023-06-07 20:38:20 +0600

    daisy Ui installed and eslintric.cjs file updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -10,6 +10,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    "react/prop-types": "off",
     'react-refresh/only-export-components': 'warn',
   },
 }
